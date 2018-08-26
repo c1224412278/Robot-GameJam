05a8eb0feb6358d40a398cd2ca93f2c4a08dfe99
Yan <s14113136@stu.edu.tw>
2018-08-26 08:05:23 +0800

    texture

--- Assets/Scripts/NPC/NPCSpots.cs
+++ Assets/Scripts/NPC/NPCSpots.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class NPCSpots : MonoBehaviour {
 
     public static NPCSpots Instance;
+
+    [Header("觀光景點")]
     public GameObject[] m_spots;
+
+    [Header("進出景點")]
+    public GameObject[] m_exitSpots;
 
     private void Awake()
     {
@@ -20,6 +25,11 @@ public class NPCSpots : MonoBehaviour {
     public Vector2 GetRandomSpot()
     {
         return m_spots[Random.Range(0, m_spots.Length)].transform.position;
+    }
+
+    public Vector2 GetRandomExitSpot()
+    {
+        return m_exitSpots[Random.Range(0, m_spots.Length)].transform.position;
     }
 
 }
