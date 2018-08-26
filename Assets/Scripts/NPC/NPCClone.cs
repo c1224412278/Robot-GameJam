262611b9c13cea81e84a9319437726f40a35e090
Yan <s14113136@stu.edu.tw>
2018-08-26 08:08:15 +0800

    one more

---
+++ Assets/Scripts/NPC/NPCClone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCClone : MonoBehaviour {
+
+    public GameObject m_NPCobj;
+    public float m_cloneTime;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+}
